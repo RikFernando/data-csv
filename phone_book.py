9561427e85788a00e0249ab333de2d9e779dc1ae
Rik Van Moll <rikvnmll@gmail.com>
2024-04-17 11:20:36 +0200

    second question

--- phone_book.py
+++ phone_book.py
@@ -1,9 +1,8 @@
-
 import csv
 
 with open('data/phone_book.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+    csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
     line_count = 0
     for row in csv_reader:
-        print(row)
+        print(f"{row['first_name']}: {row['phone_number']}")
         line_count += 1
