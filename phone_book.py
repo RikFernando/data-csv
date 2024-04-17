8613dcd66d2c0a5d3a35fac2cb4d80075c0c4b76
Rik Van Moll <rikvnmll@gmail.com>
2024-04-17 11:19:13 +0200

    first question

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,9 @@
 
+import csv
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        print(row)
+        line_count += 1
